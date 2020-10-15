4752a289c26bad5cb6234c1b0529db1719734052
nakakyon <>
2020-10-15 22:26:39 +0900

    型定義を追加

---
+++ project/src/types/nuxt.d.ts
@@ -0,0 +1,18 @@
+import VueRouter from 'vue-router'
+import moment from 'moment'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $router: VueRouter
+    $moment: typeof moment
+    $copyText: any
+    $bus: any
+  }
+}
+
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    layout?: string
+    middleware?: string | String[]
+  }
+}
